be2dab58dc118aac497714993c5e7827d184e5a1
Teutonium <rrrrrrrrm@gmail.com>
2022-02-11 17:48:13 +0000

    Ownable updated

--- migrations/1_deploy_TeutToken.js
+++ migrations/1_deploy_TeutToken.js
@@ -1,6 +1,10 @@
 const TeutToken = artifacts.require("TeutToken");
+const Ownable= artifacts.require('Ownable');
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(TeutToken, "TeutToken", "TTK",18,"5000000");
-  const teutToken = await TeutToken.deployed()
+  const teutToken = await TeutToken.deployed();
+
+
+  await deployer.deploy(Ownable);
 };
