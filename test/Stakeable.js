879fd7b55355485e7fd958fc6554090947f72127
Teutonium <rrrrrrrrm@gmail.com>
2022-02-12 22:44:46 +0000

    Staking changes

--- test/Stakeable.js
+++ test/Stakeable.js
@@ -122,6 +122,66 @@ contract("TeutToken", async accounts => {
 
         assert.equal(summary.stakes[0].user, "0x0000000000000000000000000000000000000000", "Failed to remove stake when it was empty");
     });
+
+    it("calculate rewards", async() => {
+        teutToken = await TeutToken.deployed();
+
+        let owner = accounts[0];
+
+        // Owner has 1 stake at this time, its the index 1 with 100 Tokens staked
+        // So lets fast forward time by 20 Hours and see if we gain 2% reward
+        const newBlock = await helper.advanceTimeAndBlock(3600*20);
+        let summary = await teutToken.hasStake(owner);
+
+        
+        let stake = summary.stakes[1];
+        assert.equal(stake.claimable, 100*0.02, "Reward should be 2 after staking for twenty hours with 100")
+        // Make a new Stake for 1000, fast forward 20 hours again, and make sure total stake reward is 24 (20+4)
+        // Remember that the first 100 has been staked for 40 hours now, so its 4 in rewards.
+        await teutToken.stake(1000, {from: owner});
+        await helper.advanceTimeAndBlock(3600*20);
+
+        summary = await teutToken.hasStake(owner);
+
+        stake = summary.stakes[1];
+        let newstake = summary.stakes[2];
+
+        assert.equal(stake.claimable, (100*0.04), "Reward should be 4 after staking for 40 hours")
+        assert.equal(newstake.claimable, (1000*0.02), "Reward should be 20 after staking 20 hours");
+    });
+
+    it("reward stakes", async() => {
+        teutToken = await TeutToken.deployed();
+        // Use a fresh Account, Mint 1000 Tokens to it
+        let staker = accounts[3];
+        await teutToken.mint(accounts[3],1000);
+        let initial_balance = await teutToken.balanceOf(staker);
+        // Make a stake on 200, fast forward 20 hours, claim reward, amount should be Initial balanace +4
+        await teutToken.stake(200, {from: staker});
+        await helper.advanceTimeAndBlock(3600*20);
+
+        let stakeSummary = await teutToken.hasStake(staker);
+        let stake = stakeSummary.stakes[0];
+        // Withdraw 100 from stake at index 0 
+        await teutToken.withdrawStake(100, 0, { from: staker});
+
+        // Balance of account holder should be updated by 104 tokens
+        let after_balance = await teutToken.balanceOf(staker);
+
+        let expected = 1000-200+100+Number(stake.claimable);
+        assert.equal(after_balance.toNumber(), expected, "Failed to withdraw the stake correctly")
     
+        // Claiming them again should not return any rewards since we reset timer
+    
+        try{
+            await teutToken.withdrawStake(100, 0 , {from:staker});
+        }catch(error){
+            assert.fail(error);
+        }
+        let second_balance = await teutToken.balanceOf(staker);
+        // we should have gained 100 this time.
+        assert.equal(second_balance.toNumber(), after_balance.toNumber()+100, "Failed to reset timer second withdrawal reward")
+    });
+
 
 });
